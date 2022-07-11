7c171b2126e29432e59a4e9719a2bac204652819
roynaldi19 <roynaldi19@gmail.com>
2022-07-11 09:58:23 +0700

    Task: Add a button

--- app/src/main/java/com/roynaldi19/akf01_02diceroller/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/akf01_02diceroller/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    val randomInt = (1..6).random()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun roolDice() {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        binding.resultText.text = randomInt.toString()
     }
 }
