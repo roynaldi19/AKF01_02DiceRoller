dbcf0ce123047a79508e75d427c4958054c64324
roynaldi19 <roynaldi19@gmail.com>
2022-07-11 09:53:32 +0700

    Task: Add a button

--- app/src/main/java/com/roynaldi19/akf01_02diceroller/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/akf01_02diceroller/MainActivity.kt
@@ -2,10 +2,25 @@ package com.roynaldi19.akf01_02diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.roynaldi19.akf01_02diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.roolButton.setOnClickListener {
+            roolDice()
+        }
+
+    }
+
+    private fun roolDice() {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
     }
 }
